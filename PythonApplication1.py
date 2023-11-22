af532517959b1ecbf702751d4cb952c6ec7c4a3a
Matei Popescu <matei.popescu@outlook.de>
2023-11-23 00:13:37 +0200

    Merge remote-tracking branch 'origin/master'

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,19 @@
-print("Hello")
+import pygame
+import sys
+
+pygame.init()
+screenWidth = 480
+screenHeight = 600
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Cube Game")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+screen.fill((255, 255, 255))
+pygame.display.flip()
+pygame.time.Clock().tick(60)
+
